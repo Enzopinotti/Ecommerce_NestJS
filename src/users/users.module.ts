5230aa75463016184f3a34c83be6a1c37dace4d5
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-17 13:15:51 -0300

    B2: remove duplicate ConfigModule registration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/users.schema';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { JwtStrategy } from './auth/jwt.strategy';
@@ -14,21 +12,15 @@ import { MailService } from 'src/mail/mail.service';
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: User.name,
-        schema: UserSchema
-      }
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
     ]),
     JwtModule,
-    ConfigModule.forRoot(),
   ],
   controllers: [UsersController],
-  providers: [UsersService,
-    AuthService,
-    JwtAuthGuard,
-    JwtStrategy,
-    ConfigService,
-    MailService
-  ],
+  providers: [UsersService, AuthService, JwtAuthGuard, JwtStrategy, MailService],
   exports: [UsersService],
 })
 export class UsersModule {}
