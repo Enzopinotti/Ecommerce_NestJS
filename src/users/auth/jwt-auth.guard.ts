c7b13e0c9155cb00dac22839bd572c01135b78d9
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-17 14:42:40 -0300

    B3: remove duplicate users JWT guard

--- src/users/auth/jwt-auth.guard.ts
+++
@@ -1,8 +0,0 @@
-// jwt-auth.guard.ts
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
-
-//Para usarlo en un controller poner antes despues del get y antes de la función @UseGuards(JwtAuthGuard)
